2b03bae66bff47b7500aeb3694fc035628cfbcbe
Adam <adam@atik.cc>
2021-10-31 16:53:56 -0500

    download links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import { AppProps } from "next/dist/shared/lib/router/router";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const queryClient = new QueryClient();
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,29 @@ import {
   faLinux,
   faWindows,
 } from "@fortawesome/free-brands-svg-icons";
+import { useMemo } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 const Home = () => {
+  const { data: release } = useQuery("release", async () => {
+    return (
+      await axios.get<{
+        assets: {
+          browser_download_url: string;
+          name: string;
+        }[];
+      }>("https://api.github.com/repos/skyebrowser/skye/releases/latest")
+    ).data;
+  });
+  const macDownload = useMemo(() => {
+    return release?.assets?.find((asset) => asset.name.endsWith(".dmg"));
+  }, [release]);
+  const winDownload = useMemo(() => {
+    return release?.assets?.find((asset) => asset.name.endsWith(".exe"));
+  }, [release]);
+  const linuxDownload = useMemo(() => {
+    return release?.assets?.find((asset) => asset.name.endsWith(".AppImage"));
+  }, [release]);
   return (
     <>
       <div className="relative flex justify-center items-center">
@@ -25,9 +47,12 @@ const Home = () => {
             <br />
             wasn’t absolute shit
           </h1>
-          <button className="mt-5 p-3 bg-primary rounded-lg w-60">
-            Download for macOS
-          </button>
+          <a
+            className="mt-5 p-3 bg-primary rounded-lg w-60"
+            href={"/#downloads"}
+          >
+            Download Skye
+          </a>
         </div>
         <img className="bottom-0 ml-auto  header-image" src="/preview.png" />
       </div>
@@ -104,7 +129,10 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center bg-blue-50 dark:bg-gray-900 dark:text-white">
+      <div
+        id="downloads"
+        className="flex flex-col items-center bg-blue-50 dark:bg-gray-900 dark:text-white"
+      >
         <div className="m-10 md:m-20 max-w-7xl md:w-full">
           <div className="w-100">
             <h1 className="uppercase">Downloads</h1>
@@ -114,15 +142,24 @@ const Home = () => {
           </div>
           <div className="w-100">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
-              <button className="w-100 bg-black text-white p-5 rounded-lg">
-                Download for macOS <FontAwesomeIcon icon={faApple} />
-              </button>
-              <button className="w-100 bg-blue-400 text-white p-5 rounded-lg">
-                Download for Windows <FontAwesomeIcon icon={faWindows} />
-              </button>
-              <button className="w-100 bg-yellow-300 text-black p-5 rounded-lg">
-                Download for Linux <FontAwesomeIcon icon={faLinux} />
-              </button>
+              <a
+                href={macDownload?.browser_download_url}
+                className="w-100 bg-black text-white p-5 rounded-lg flex items-center justify-center gap-2"
+              >
+                <p>Download for macOS</p> <FontAwesomeIcon icon={faApple} />
+              </a>
+              <a
+                href={winDownload?.browser_download_url}
+                className="w-100 bg-blue-400 text-white p-5 rounded-lg flex items-center justify-center gap-2"
+              >
+                <p>Download for Windows</p> <FontAwesomeIcon icon={faWindows} />
+              </a>
+              <a
+                href={linuxDownload?.browser_download_url}
+                className="w-100 bg-yellow-300 text-black p-5 rounded-lg flex items-center justify-center gap-2"
+              >
+                <p>Download for Linux</p> <FontAwesomeIcon icon={faLinux} />
+              </a>
             </div>
           </div>
         </div>
